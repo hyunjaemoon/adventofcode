09197eda2b5c8dec0f7b195395abc1b0cac11746
Hyun Jae Moon <hyunjaemoon@google.com>
2023-12-04 16:04:30 -0800

    Skeleton code for day 3 and day 4

---
+++ rust_solutions/src/day4/mod.rs
@@ -0,0 +1,24 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
+
+fn read_file() -> BufReader<File> {
+    // Open the file for reading
+    let mut file_dir = PathBuf::new();
+    file_dir.push("src");
+    file_dir.push("day4");
+    file_dir.push("input.txt");
+    let file = File::open(file_dir).expect("Failed to open file");
+    BufReader::new(file)
+}
+
+pub fn part1() {
+    // Create a buffered reader to read the file
+    let reader = read_file();
+
+    for line in reader.lines() {
+        println!("{line:?}");
+    }
+}
